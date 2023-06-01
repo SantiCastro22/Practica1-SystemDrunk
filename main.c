7b7d87948294260185da189e6bc51840c5f41016
Santiago Castro <santicastro0128@gmail.com>
2023-05-31 23:34:17 -0300

    Revision final y correcciones

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAXLIB 200
+#define MAXLIB 200 // CANTIDAD MAXIMA DE LIBROS
 
 typedef struct
 {
@@ -15,11 +15,12 @@ typedef struct
 
 // VARIABLES GLOBALES
 Libro registroLibros[MAXLIB]; // ARRAY DE TIPO LIBRO
-int contLibros = 0;
+int contLibros = 0;			  // CONTADOR DE LIBROS CARGADOS
 
 void agregarLibro(Libro registroLibros[MAXLIB]);
 void eliminarLibro(Libro registroLibros[MAXLIB]);
 void mostrarLibro(Libro registroLibros[MAXLIB]);
+void modificarLibro(Libro registroLibros[MAXLIB]);
 
 int main()
 {
@@ -51,61 +52,63 @@ int main()
 			mostrarLibro(registroLibros);
 			break;
 		case 5:
-			printf("Saliendo del sistema...\n");
+			printf("\n Saliendo del sistema...\n");
 			break;
 
 		default:
-			printf("Por favor, ingrese una opcion valida.\n");
+			printf("\n Por favor, ingrese una opcion valida.\n");
 			break;
 		}
 		printf("\n");
 	} while (opcion != 5);
-	
+
 	return 0;
 }
 
 void agregarLibro(Libro registroLibros[MAXLIB])
 {
-    printf("\n------ AGREGAR NUEVO LIBRO ------\n");
+	printf("\n------ AGREGAR NUEVO LIBRO ------\n");
 
-    int opcion;
+	int opcion;
 
-    do
-    {
-        fflush(stdin);
-        printf("- Autor del libro: ");
-        gets(registroLibros[contLibros].autor);
-        printf("- Título del libro: ");
-        gets(registroLibros[contLibros].titulo);
-        printf("- Editorial del libro: ");
-        gets(registroLibros[contLibros].editorial);
-        printf("- Año de publicación del libro: ");
-        scanf("%d", &registroLibros[contLibros].anio);
-        printf("- Precio del libro: ");
-        scanf("%f", &registroLibros[contLibros].precio);
+	do
+	{
+		fflush(stdin);
+		printf("Libro Nro [%d]\n", contLibros + 1);
+		printf("- Autor del libro: ");
+		gets(registroLibros[contLibros].autor);
+		printf("- Titulo del libro: ");
+		gets(registroLibros[contLibros].titulo);
+		printf("- Editorial del libro: ");
+		gets(registroLibros[contLibros].editorial);
+		printf("- A%co de publicacion del libro: ", 164);
+		scanf("%d", &registroLibros[contLibros].anio);
+		printf("- Precio del libro: ");
+		scanf("%f", &registroLibros[contLibros].precio);
 
-        contLibros++; // Incrementar la cantidad de libros
+		contLibros++; // Incrementar la cantidad de libros
 
-        printf("¿Desea agregar otro libro? (1: Sí / 0: No): ");
-        scanf("%d", &opcion);
-        printf("\n");
-    } while (opcion == 1);
+		printf("Desea agregar otro libro? (1: SI / 0: NO): ");
+		scanf("%d", &opcion);
+		printf("\n");
+	} while (opcion == 1);
 }
 
 void mostrarLibro(Libro registroLibros[MAXLIB])
 {
 	printf("\nREGISTRO DE LIBROS EXISTENTES\n");
 
-	for (int i = 0; i < MAXLIB; i++)
+	for (int i = 0; i < contLibros; i++)
 	{
 		printf("Libro [%d]\n", i + 1);
 		printf("Titulo: %s\n", registroLibros[i].titulo);
 		printf("Autor: %s\n", registroLibros[i].autor);
 		printf("Editorial: %s\n", registroLibros[i].editorial);
 		printf("A%co: %d\n", 164, registroLibros[i].anio);
-		printf("Precio: %.2f\n", registroLibros[i].precio);
+		printf("Precio: $ %.2f\n", registroLibros[i].precio);
 		printf("----------------------\n");
 	}
+	system("pause");
 }
 
 void eliminarLibro(Libro registroLibros[MAXLIB])
@@ -114,14 +117,15 @@ void eliminarLibro(Libro registroLibros[MAXLIB])
 	int encontrado = 0;
 	fflush(stdin);
 	printf("\nELIMINAR LIBROS\n");
+	printf("Libros Existentes: %d \n", contLibros);
 	printf("Ingrese el titulo del libro a eliminar: ");
 	gets(indexTituloLibro);
 
-	for (int i = 0; i < MAXLIB; i++)
+	for (int i = 0; i < contLibros; i++)
 	{
 		if (strcmp(indexTituloLibro, registroLibros[i].titulo) == 0)
 		{
-			for (int j = i; j < MAXLIB; j++)
+			for (int j = i; j < contLibros; j++)
 				registroLibros[j] = registroLibros[j + 1];
 
 			encontrado = 1;
@@ -133,34 +137,38 @@ void eliminarLibro(Libro registroLibros[MAXLIB])
 	{
 		printf("\nEl libro que desea eliminar no se encuentra\n");
 	}
+
+	system("pause");
 }
 
 void modificarLibro(Libro registroLibros[MAXLIB])
 {
-    int editLibro; 
-    printf("\nMODIFICAR LIBROS\n");
-    printf("Ingrese el numero de libro a editar: ");
-    scanf("%d", &editLibro);
+	int editLibro;
+	printf("\nMODIFICAR LIBROS\n");
+	printf("Ingrese el numero de libro a editar: ");
+	scanf("%d", &editLibro);
 
-    editLibro--;
-    fflush(stdin);
+	editLibro--;
+	fflush(stdin);
 
-    if (editLibro <= contLibros)
-    {
-        printf("Nuevo Autor: ");
-        gets(registroLibros[editLibro].autor);
-        printf("Nuevo Titulo: ");
-        gets(registroLibros[editLibro].titulo);
-        printf("Nuevo Editorial: ");
-        gets(registroLibros[editLibro].editorial);
-        printf("Nuevo A%co de publicacion: ",164);
-        scanf("%d",&registroLibros[editLibro].anio);
-        printf("Nuevo Precio: ");
-        scanf("%f",&registroLibros[editLibro].precio);
-        printf("\nEl libro se modifico con exito\n");
-    }
-    else 
-        printf("\nEl libro que desea modicar no se encuentra\n");
-    
+	if (editLibro <= contLibros)
+	{
+		printf("Nuevo Autor: ");
+		gets(registroLibros[editLibro].autor);
+		printf("Nuevo Titulo: ");
+		gets(registroLibros[editLibro].titulo);
+		printf("Nuevo Editorial: ");
+		gets(registroLibros[editLibro].editorial);
+		printf("Nuevo A%co de publicacion: ", 164);
+		scanf("%d", &registroLibros[editLibro].anio);
+		printf("Nuevo Precio: ");
+		scanf("%f", &registroLibros[editLibro].precio);
+		printf("\nEl libro se modifico con exito\n");
+	}
+	else
+	{
+		printf("\nEl libro que desea modicar no se encuentra\n");
+	}
+
+	system("pause");
 }
-
